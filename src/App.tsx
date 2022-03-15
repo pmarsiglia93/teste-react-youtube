473064fadb04e9bf8356682ea13071003d4749e3
Paulo <pmarsiglia93@gmail.com>
2022-03-15 19:56:12 -0300

    pmarsiglia93 [TASK: REACT NO YOUTUBE]

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet"
+
+function App() {
+  const [tweets, setTweets] = useState<string[]>([])
+
+  return(
+    <div>
+      <Tweet text="Tweet 1"/>
+      <Tweet text="Tweet 2"/>
+      <Tweet text="Tweet 3"/>
+
+      <button>Adicionar Tweet</button>
+    </div>
+  );  
+}
+
+export default App
